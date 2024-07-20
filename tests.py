4ba7f314c7ca4faed0eddd166f6ba84030724c74
indexerror <lkycst@gmail.com>
2024-07-20 21:37:55 +0700

    blank test again

--- tests.py
+++ tests.py
@@ -1,6 +1,2 @@
-import numpy as np
-from svcvxcluster.benchmark.halfmoon import benchmark_moons
-
 if __name__ == "__main__":
-    times = benchmark_moons(1000, [0.05], np.arange(0.2, 2.2, 0.2))
-    print(times.describe())
+    ...
