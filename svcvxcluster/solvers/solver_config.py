810e6c73e09b5533178776df6e5ce67396f3d5fc
indexerror <lkycst@gmail.com>
2024-07-30 15:21:52 +0700

    change the default parameter, clean tests.py

--- svcvxcluster/solvers/solver_config.py
+++ svcvxcluster/solvers/solver_config.py
@@ -13,7 +13,7 @@ class SolverConfig:
     armijo_beta: float = 0.75
     armijo_iter: int = 10
     mu_update_tol: float = 1
-    mu_update_tol_decay: float = 0.95
+    mu_update_tol_decay: float = 0.75
     max_iter: int = 1000
     mu_min: float = 1e-5
     mu_max: float = 1e5
--- tests.py
+++ tests.py
@@ -1,16 +1,16 @@
 from svcvxcluster import SvCvxCluster
-from svcvxcluster.benchmark.large_dataset import load_large
+from svcvxcluster.benchmark.halfmoon import load_moons
 from sklearn.metrics import adjusted_rand_score
 from time import perf_counter_ns
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    X, y = load_large(50000, 10)
+    X, y = load_moons(100000)
     clust = SvCvxCluster(10, alpha=0.4, alpha_prime=1)
     t = perf_counter_ns()
     clust.fit(X)
     print("Solve Time:", (perf_counter_ns() - t) / 1e9)
     print("Adjuste Rand Score:", adjusted_rand_score(y, clust.labels_))
-    plt.scatter(*X[:, :2].T, c=clust.labels_)
+    plt.scatter(*X.T, c=clust.labels_)
     plt.show()
     
